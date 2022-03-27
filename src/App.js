c2306f42b650578097824f75af566bc415a2b214
Leonid Lichkun <lionproklon@gmail.com>
2022-03-27 22:55:04 +0300

    Added responsive AppBar, substitute navBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
-import NavTabs from "./components/ui/navItems/navTabs";
+import ResponsiveAppBar from "./components/responsiveAppBar";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
+import Other from "./layouts/other";
 
 function App() {
   return (
     <>
-      <NavTabs />
+      <ResponsiveAppBar />
       <Switch>
         <Route path="/login/:type?" component={Login} />
+        <Route path="/other" component={Other} />
         <Route path="/" exact component={Main} />
         <Redirect to="/" />
       </Switch>
