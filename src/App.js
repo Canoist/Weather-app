89be79311f574996e8d27a2c9c2ac46727feffe4
Leonid Lichkun <lionproklon@gmail.com>
2022-03-27 21:36:06 +0300

    Changed main page, added login page, changed app.js, added nav items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import NavTabs from "./components/ui/navItems/navTabs";
+import Login from "./layouts/login";
 import Main from "./layouts/main";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={Main} />
-      <Redirect to="/" />
-    </Switch>
+    <>
+      <NavTabs />
+      <Switch>
+        <Route path="/login/:type?" component={Login} />
+        <Route path="/" exact component={Main} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,38 +1,6 @@
-import { Box } from "@mui/material";
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import SignInForm from "../components/ui/signInForm";
-import ValidateForm from "../components/ui/validateForm";
+import React from "react";
 
 const Main = () => {
-  const { type } = useParams();
-  const [formType, setFormType] = useState(
-    type === "register" ? type : "login"
-  );
-  const toggleFormType = () => {
-    setFormType((prev) => (prev === "register" ? "login" : "register"));
-  };
-
-  return (
-    <Box
-      sx={{
-        width: "100vw",
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      {formType === "register" ? (
-        <>
-          <ValidateForm toggleForm={toggleFormType} />
-        </>
-      ) : (
-        <>
-          <SignInForm toggleForm={toggleFormType} />
-        </>
-      )}
-    </Box>
-  );
+  return <h1>Main Page</h1>;
 };
 export default Main;
