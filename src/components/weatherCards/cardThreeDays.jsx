aaf530b806fa68bfebda39528a679e88dd77eb28
Leonid Lichkun <lionproklon@gmail.com>
2022-04-12 11:41:33 +0300

    Added card with 3 days of weather forecast, refactor of weatherWind.jsx

--- src/components/weatherCards/cardThreeDays.jsx
+++ src/components/weatherCards/cardThreeDays.jsx
@@ -1,42 +1,18 @@
 import React from "react";
-import { Box, Card, CardContent } from "@mui/material";
-import WeatherWind from "./weatherWind";
+import { Card, Divider } from "@mui/material";
 import { useWeather } from "../../hooks/useWeather";
-import WeatherIconAndDescription from "./weatherIconAndDescription";
-import DayWeekDescription from "./dayWeekDescription";
+import CardOneOfThreeDays from "./cardOneOfThreeDays";
 
 const CardThreeDays = () => {
     const { weather } = useWeather();
     const { daily } = weather;
     return (
-        <Card elevation={3} sx={{ maxWidth: 345 }}>
-            <CardContent sx={{ textAlign: "center" }}>
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-around"
-                    }}
-                >
-                    <WeatherIconAndDescription weather={daily[0].weather[0]} />
-                    <DayWeekDescription timestamp={daily[0].dt} />
-                    {/* СДЕЛАТЬ ДРУГОЙ КОМПОНЕНТ
-                    <Box sx={{ textAlign: "left" }}>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {Math.trunc(weather.temp)} {"\u2103"}
-                        </Typography>
-                        <Typography
-                            gutterBottom
-                            variant="body1"
-                            component="div"
-                        >
-                            Ощущается как {Math.trunc(weather.feels_like)}{" "}
-                            {"\u2103"}
-                        </Typography>
-                    </Box> */}
-                </Box>
-                <WeatherWind data={weather} />
-            </CardContent>
+        <Card elevation={3} sx={{ mt: 2, maxWidth: 1050, display: "flex" }}>
+            <CardOneOfThreeDays day={daily[0]} />
+            <Divider orientation="vertical" flexItem />
+            <CardOneOfThreeDays day={daily[1]} />
+            <Divider orientation="vertical" flexItem />
+            <CardOneOfThreeDays day={daily[2]} />
         </Card>
     );
 };
--- src/components/weatherCards/weatherWind.jsx
+++ src/components/weatherCards/weatherWind.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Box, SvgIcon } from "@mui/material";
 import { getWindDirection } from "../../utils/getWindDirection";
-import { useWeather } from "../../hooks/useWeather";
 
 const WeatherWind = ({ data }) => {
-    const { weather } = useWeather();
-    const currentWeather = weather.current;
     return (
         <Box
             sx={{
@@ -35,17 +32,17 @@ const WeatherWind = ({ data }) => {
                     sx={{
                         mr: 0.5,
                         fontSize: "16px",
-                        transform: `rotate(${currentWeather.wind_deg}deg)`
+                        transform: `rotate(${data.wind_deg}deg)`
                     }}
                 >
                     <g fill="#48484a">
                         <path d="M510.5,749.6c-14.9-9.9-38.1-9.9-53.1,1.7l-262,207.3c-14.9,11.6-21.6,6.6-14.9-11.6L474,48.1c5-16.6,14.9-18.2,21.6,0l325,898.7c6.6,16.6-1.7,23.2-14.9,11.6L510.5,749.6z"></path>
                     </g>
                 </SvgIcon>
-                {Math.trunc(currentWeather.wind_speed) > 0
-                    ? Math.trunc(currentWeather.wind_speed)
+                {Math.trunc(data.wind_speed) > 0
+                    ? Math.trunc(data.wind_speed)
                     : 1}{" "}
-                м/с {getWindDirection(currentWeather.wind_deg)}
+                м/с {getWindDirection(data.wind_deg)}
             </Box>
         </Box>
     );
